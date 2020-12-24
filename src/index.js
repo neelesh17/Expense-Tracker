70c4cd05fe9b091bbc08ff5fc53471fb6dcfa0e1
Neelesh Thakur <nt00605@gmail.com>
2020-12-24 12:05:59 +0530

    changed service worker and test files

--- src/index.js
+++ src/index.js
@@ -7,11 +7,15 @@ import * as serviceWorker from './serviceWorker';
 import './index.css';
 
 ReactDOM.render(
-  <SpeechProvider appId="4e202d14-992b-4af0-81cb-a72bee726f6f" language="en-US">
-    <Provider>
-      <App />
-    </Provider>
-  </SpeechProvider>,
+    <SpeechProvider appId="4e202d14-992b-4af0-81cb-a72bee726f6f" language="en-US">
+      <Provider>
+        <App />
+      </Provider>
+    </SpeechProvider>,
   document.getElementById('root')
 );
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.register();
