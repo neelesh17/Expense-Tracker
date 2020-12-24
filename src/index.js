8faab2b464f75774d34cc1db579e445952b9bef8
Neelesh Thakur <nt00605@gmail.com>
2020-12-24 12:24:22 +0530

    removed service workee not getting added bug by changing react version to 16.13.1

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import { Provider } from './context/context';
 import { SpeechProvider } from  '@speechly/react-client';
-import * as serviceWorker from './serviceWorker';
 import './index.css';
+import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
     <SpeechProvider appId="4e202d14-992b-4af0-81cb-a72bee726f6f" language="en-US">
